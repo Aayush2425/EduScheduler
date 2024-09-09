fe916c942d0fb827380ab5f441128ee8dd06a5cb
Aayush2425 <122793682+Aayush2425@users.noreply.github.com>
2024-09-09 19:07:33 +0530

    hello admin

--- server/controller/AdminController.js
+++ server/controller/AdminController.js
@@ -1,95 +1,120 @@
-const bcrypt = require('bcryptjs')
-const jwt = require('jsonwebtoken')
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 const Admin = require("../model/AdminModel.js");
+
+// get all details of admin by id
+const getAdmin = async (req, res) => {
+  const adminId = req.params.id;
+
+  try {
+    const admin = await Admin.findById(adminId);
+    if (!admin) {
+      return res.status(404).json({ message: "Admin not found" });
+    }
+    res.status(200).json(admin);
+  } catch (error) {
+    res
+      .status(500)
+      .json({ error: "Error getting admin", details: error.message });
+  }
+};
 
 // Update Admin Details
 const updateAdmin = async (req, res) => {
-    const adminId = req.params.id;
+  const adminId = req.params.id;
 
-    //  universityEmail , username , password , personalUniEmail
-    const updates = req.body;
+  //  universityEmail , username , password , personalUniEmail
+  const updates = req.body;
 
-    try {
-        const updatedAdmin = await Admin.findByIdAndUpdate(adminId, updates, {
-            new: true,
-        });
-        if (!updatedAdmin) {
-            return res.status(404).json({ message: "Admin not found" });
-        }
-        res.status(200).json(updatedAdmin);
-    } catch (error) {
-        res
-            .status(500)
-            .json({ error: "Error updating admin", details: error.message });
+  try {
+    const updatedAdmin = await Admin.findByIdAndUpdate(adminId, updates, {
+      new: true,
+    });
+    if (!updatedAdmin) {
+      return res.status(404).json({ message: "Admin not found" });
     }
+    res.status(200).json(updatedAdmin);
+  } catch (error) {
+    res
+      .status(500)
+      .json({ error: "Error updating admin", details: error.message });
+  }
 };
 
 const signupAdmin = async (req, res) => {
-    try {
-        const adminExist = await Admin.findOne({ personalUniEmail: personalUniEmail })
+  try {
+    const adminExist = await Admin.findOne({
+      personalUniEmail: personalUniEmail,
+    });
 
-        if (adminExist) {
-            console.log('admin exist!!!');
-            return res.status(422).json({ error: 'Admin already exists' });
-        }
-
-        let hashPassword = await bcrypt.hash(password, 12);
-        password = hashPassword;
-
-        const newAdmin = await Admin.create({ username, password, universityEmail, personalUniEmail });
-
-        const token = jwt.sign({ _id: newAdmin._id }, process.env.SECRET_KEY, {
-            expiresIn: "1h",
-        });
-        newAdmin.token = token;
-        newAdmin.save();
-
-        res.cookie('token', token, {
-            httpOnly: true,
-            secure: true,
-            maxAge: 3600000
-        });
-
-        return res.status(200).send({ data: newAdmin });
-    } catch (err) {
-        console.log(err);
-        return res.status(501).send(err);
+    if (adminExist) {
+      console.log("admin exist!!!");
+      return res.status(422).json({ error: "Admin already exists" });
     }
+
+    let hashPassword = await bcrypt.hash(password, 12);
+    password = hashPassword;
+
+    const newAdmin = await Admin.create({
+      username,
+      password,
+      universityEmail,
+      personalUniEmail,
+    });
+
+    const token = jwt.sign({ _id: newAdmin._id }, process.env.SECRET_KEY, {
+      expiresIn: "1h",
+    });
+    newAdmin.token = token;
+    newAdmin.save();
+
+    res.cookie("token", token, {
+      httpOnly: true,
+      secure: true,
+      maxAge: 3600000,
+    });
+
+    return res.status(200).send({ data: newAdmin });
+  } catch (err) {
+    console.log(err);
+    return res.status(501).send(err);
+  }
 };
 
 const signinAdmin = async (req, res) => {
-    let { personalUniEmail, password } = req.body;
+  let { personalUniEmail, password } = req.body;
 
-    try {
-        if (!personalUniEmail || !password) {
-            return res.status(422).json({ message: 'please enter required details' })
-        }
-
-        const loginAdmin = await Admin.findOne({ personalUniEmail: personalUniEmail });
-
-        if (!loginAdmin) {
-            return res.status(400).send({ message: 'Invalid details' });
-        }
-
-        const isMatch = await bcrypt.compare(password, loginAdmin.password);
-
-        if (!isMatch) {
-            return res.status(400).send({ message: 'Invalid details' });
-        }
-        const token = jwt.sign({ _id: loginAdmin._id }, process.env.SECRET_KEY, {
-            expiresIn: '1h',
-            maxAge: 3600000,
-        });
-
-        loginAdmin.token = token;
-        await loginAdmin.save();
-
-        return res.status(200).send({ data: loginAdmin });
+  try {
+    if (!personalUniEmail || !password) {
+      return res.status(422).json({ message: "please enter required details" });
     }
-    catch (error) {
-        console.log(err);
-        return res.status(501).send(err);
+
+    const loginAdmin = await Admin.findOne({
+      personalUniEmail: personalUniEmail,
+    });
+
+    if (!loginAdmin) {
+      return res.status(400).send({ message: "Invalid details" });
     }
-}
+
+    const isMatch = await bcrypt.compare(password, loginAdmin.password);
+
+    if (!isMatch) {
+      return res.status(400).send({ message: "Invalid details" });
+    }
+    const token = jwt.sign({ _id: loginAdmin._id }, process.env.SECRET_KEY, {
+      expiresIn: "1h",
+      maxAge: 3600000,
+    });
+
+    loginAdmin.token = token;
+    await loginAdmin.save();
+
+    return res.status(200).send({ data: loginAdmin });
+  } catch (error) {
+    console.log(err);
+    return res.status(501).send(err);
+  }
+};
 
 module.exports = { signupAdmin, signinAdmin, updateAdmin };
--- server/routes/AdminRouter.js
+++ server/routes/AdminRouter.js
@@ -1,9 +1,16 @@
-const express = require('express');
+const express = require("express");
+const { updateAdmin, getAdmin } = require("../controller/AdminController.js");
 const router = express.Router();
-const { signupAdmin, signinAdmin, updateAdmin } = require('../controller/AdminController');
+const {
+  signupAdmin,
+  signinAdmin,
+  updateAdmin,
+} = require("../controller/AdminController");
 
-router.post('/signup', signupAdmin)
-router.post('/signin', signinAdmin)
+// Update Admin
+router.get("/", getAdmin);
+router.post("/signup", signupAdmin);
+router.post("/signin", signinAdmin);
 router.put("/:id", updateAdmin);
 
 module.exports = router;
