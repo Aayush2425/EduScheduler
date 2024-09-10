4aa48fae6e27db7fd3d9495101a9fb5f06b94725
Aayush2425 <122793682+Aayush2425@users.noreply.github.com>
2024-09-10 16:55:01 +0530

    Aayush Changed

--- server/routes/FacultyRouter.js
+++ server/routes/FacultyRouter.js
@@ -1,9 +1,13 @@
 const express = require("express");
-const { updateFaculty, getAllFaculty, createFaculty } = require("../controller/FacultyController.js");
+const {
+  updateFaculty,
+  getAllFaculty,
+  createFaculty,
+} = require("../controller/FacultyController.js");
 const router = express.Router();
 
 // Update Faculty
-router.post("/createfaculty", createFaculty)
+router.post("/createfaculty", createFaculty);
 router.put("/:id", updateFaculty);
 router.get("/:uniName/get-all-faculty", getAllFaculty);
 
